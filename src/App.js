7c62379ec93e01e024a71f99867ebd5cc7bc72ba
Truong Thien 1808 <truongvanthien593@gmail.com>
2024-08-26 10:40:21 +0700

    setup app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 
-import './App.css';
+import './App.scss';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import HomePage from './page/HomePage/HomePage';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="" className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<HomePage />}></Route>
+      </Routes>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
+    </Router>
+
   );
 }
 
---
+++ src/page/HomePage/HomePage.js
@@ -0,0 +1,12 @@
+import './HomePage.scss'
+
+const HomePage = () => {
+    return (
+        <div className="home-page-container">
+            Hello
+        </div>
+    )
+}
+
+
+export default HomePage
