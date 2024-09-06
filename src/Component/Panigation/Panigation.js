51b5ed534b2760c37d9e087c7b49c4fbc6077c34
thanh280503 <thanhhtc2805@gmail.com>
2024-09-06 09:23:46 +0700

    create Panigation

---
+++ src/Component/Panigation/Panigation.js
@@ -0,0 +1,30 @@
+import ReactPaginate from 'react-paginate';
+
+const Panigation = ({maxPage, handlePageClick}) => {
+    return (
+        <div className="panigate" style={{display: "flex", justifyContent: "center"}}>
+            <ReactPaginate
+                nextLabel="next >"
+                onPageChange={handlePageClick}
+                pageRangeDisplayed={3}
+                marginPagesDisplayed={2}
+                pageCount={maxPage}
+                previousLabel="< previous"
+                pageClassName="page-item"
+                pageLinkClassName="page-link"
+                previousClassName="page-item"
+                previousLinkClassName="page-link"
+                nextClassName="page-item"
+                nextLinkClassName="page-link"
+                breakLabel="..."
+                breakClassName="page-item"
+                breakLinkClassName="page-link"
+                containerClassName="pagination"
+                activeClassName="active"
+                renderOnZeroPageCount={null}
+            />
+        </div>
+    )
+}
+
+export default Panigation;
